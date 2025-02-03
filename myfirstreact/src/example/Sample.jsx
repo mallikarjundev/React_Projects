7cbdb45550dbfee2278d4bce1211a8b61c4a864d
arjunoggu <mallikarjundev2@gmail.com>
2025-02-03 15:14:22 -0500

    created Nested components

---
+++ myfirstreact/src/example/Sample.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Mango = () => {
+  return <h2>Mangoes are sweet</h2>;
+};
+
+const Grapes = () => {
+  return <h2>Grapes are good.</h2>;
+};
+
+function Sample() {
+  return (
+    <div>
+      Sample
+      <Mango></Mango>
+      <Grapes />
+    </div>
+  );
+}
+
+export default Sample;
