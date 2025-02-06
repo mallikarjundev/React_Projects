e1c07ba61d6a237747bfb5786262cadf7cb5717d
arjunoggu <mallikarjundev2@gmail.com>
2025-02-06 18:39:40 -0500

    passing props b/w components

--- Tic-tac-toe/src/components/GameBoard.jsx
+++ Tic-tac-toe/src/components/GameBoard.jsx
@@ -6,21 +6,23 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
-  const [GameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
       const updatedBoard = [
         ...prevGameBoard.map((innerArray) => [...innerArray]),
       ];
-      updatedBoard[rowIndex][colIndex] = "X";
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
+
+    onSelectSquare();
   }
   return (
     <ol id="game-board">
-      {GameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
--- Tic-tac-toe/src/components/Player.jsx
+++ Tic-tac-toe/src/components/Player.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
-  let editablePlayerName = <span className="player-name">{playerName}</span>;
-  let btnCaption = "Edit";
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
@@ -14,20 +12,23 @@ export default function Player({ initialName, symbol }) {
     setPlayerName(event.target.value);
   }
 
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  //   let btnCaption = "Edit";
+
   if (isEditing) {
     editablePlayerName = (
       <input type="text" required value={playerName} onChange={handleChange} />
     );
-    btnCaption = "Save";
+    // btnCaption = "Save";
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{btnCaption}</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
