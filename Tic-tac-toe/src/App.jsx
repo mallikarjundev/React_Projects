e6ef8c5cae9afd6ccc1812939e5686f64d497e04
arjunoggu <mallikarjundev2@gmail.com>
2025-02-07 13:43:52 -0500

    added draw conditiion

--- Tic-tac-toe/src/App.jsx
+++ Tic-tac-toe/src/App.jsx
@@ -54,6 +54,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
@@ -81,7 +83,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {winner && <GameOver winner={winner} />}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
