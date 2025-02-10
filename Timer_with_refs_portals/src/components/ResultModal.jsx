ef0b31410c7721616225ae24e8d4010c78ee873f
arjunoggu <mallikarjundev2@gmail.com>
2025-02-10 17:29:08 -0500

    added scores

--- Timer_with_refs_portals/src/components/ResultModal.jsx
+++ Timer_with_refs_portals/src/components/ResultModal.jsx
@@ -4,9 +4,12 @@ const ResultModal = forwardRef(function ResultModal(
   { targetTime, remainingTime, onReset },
   ref
 ) {
+  const dialog = useRef();
+
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
-  const dialog = useRef();
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
   useImperativeHandle(ref, () => {
     return {
       open() {
@@ -17,6 +20,7 @@ const ResultModal = forwardRef(function ResultModal(
   return (
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         Target tiem was <strong>{targetTime} seconds.</strong>
       </p>
