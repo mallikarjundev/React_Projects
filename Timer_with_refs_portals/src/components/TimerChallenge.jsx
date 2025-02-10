ae1466d5e33a33fc3b4b21460b88a58a6a72c881
arjunoggu <mallikarjundev2@gmail.com>
2025-02-10 14:53:37 -0500

    added start and stop timer methods

--- Timer_with_refs_portals/src/components/TimerChallenge.jsx
+++ Timer_with_refs_portals/src/components/TimerChallenge.jsx
@@ -1,14 +1,37 @@
+import { useRef, useState } from "react";
+
 export default function TimeChallenge({ title, tagetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const timer = useRef();
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, tagetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {tagetTime} second{tagetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running... / Timer inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
     </section>
   );
 }
