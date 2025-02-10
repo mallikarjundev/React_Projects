7656cd66b394b9c8871d243f0f47d991b8b741d9
arjunoggu <mallikarjundev2@gmail.com>
2025-02-10 16:50:42 -0500

    updated logic to stop and auto stop when timer is stopped/button is clicked for stop

--- Timer_with_refs_portals/src/components/TimerChallenge.jsx
+++ Timer_with_refs_portals/src/components/TimerChallenge.jsx
@@ -5,19 +5,24 @@ export default function TimeChallenge({ title, tagetTime }) {
   const timer = useRef();
   const dialog = useRef();
 
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(tagetTime * 1000);
+  const timerIsActive = timeRemaining > 0 && timeRemaining < tagetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    setTimeRemaining(tagetTime * 1000);
+    dialog.current.open();
+  }
 
   function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.open();
-    }, tagetTime * 1000);
-    setTimerStarted(true);
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
   }
 
   return (
@@ -30,12 +35,12 @@ export default function TimeChallenge({ title, tagetTime }) {
           {tagetTime} second{tagetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running..." : "Timer inactive"}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running..." : "Timer inactive"}
         </p>
       </section>
     </>
