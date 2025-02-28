6936766f38705e74abb7afa5ec58a6be51138a4b
arjunoggu <mallikarjundev2@gmail.com>
2025-02-27 20:45:55 -0500

    added better alternate for error handling

--- Advance_Routing/frontend/src/pages/Events.js
+++ Advance_Routing/frontend/src/pages/Events.js
@@ -22,7 +22,8 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    return { isError: true, message: "Could not fetch events." };
+    // return { isError: true, message: "Could not fetch events." };
+    throw { message: "Could not fetch events." };
   } else {
     return response;
   }
