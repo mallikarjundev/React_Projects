8662c536bddab818b320c485b9f44ecded728414
arjunoggu <mallikarjundev2@gmail.com>
2025-02-27 20:41:17 -0500

    added error scenario

--- Advance_Routing/frontend/src/pages/Events.js
+++ Advance_Routing/frontend/src/pages/Events.js
@@ -3,7 +3,12 @@ import { useLoaderData } from "react-router-dom";
 
 function EventsPage() {
   const data = useLoaderData();
+
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
   const events = data.events;
+
   return (
     <>
       <EventsList events={events} />
@@ -17,7 +22,7 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    //
+    return { isError: true, message: "Could not fetch events." };
   } else {
     return response;
   }
